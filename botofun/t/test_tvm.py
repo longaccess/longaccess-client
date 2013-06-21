0ea3c8aa7bb468ae3a31fad3102a86773dbae947
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2013-06-21 17:07:26 +0300

    forgot old module name

--- botofun/t/test_tvm.py
+++ botofun/t/test_tvm.py
@@ -3,8 +3,8 @@ from testtools.matchers import Equals
 
 class TVMTest(TestCase):
     def _makeit(self, *args ,**kw):
-        import test.tvm
-        return test.tvm.mytvm(*args, **kw)
+        import botofun.tvm
+        return botofun.tvm.mytvm(*args, **kw)
 
     def test_tvm(self):
         foo = self._makeit()
