e21f43ac3bbf3d9abf0faf4a9ae83b30e57a879c
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2013-08-29 15:11:58 +0300

    implement home directory step

--- behave_expect/expect/steps.py
+++ behave_expect/expect/steps.py
@@ -3,7 +3,9 @@ from behave import step
 
 @step(u'the home directory is "{dir}"')
 def home_directory(context, dir):
-    assert False
+    import os
+    os.environ['HOME'] = dir
+    assert os.path.isdir(dir), "Home directory exists"
 
 
 @step(u'I run "{command}"')
