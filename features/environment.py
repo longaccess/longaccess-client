7719522c5fa60c73e3a83bb11c94dad72029f013
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2013-08-30 17:58:56 +0300

    support temporary files in features

--- features/environment.py
+++ features/environment.py
@@ -6,6 +6,8 @@ def before_all(context):
     benv.before_all(context)
     if not context.config.log_capture:
         logging.basicConfig(level=logging.DEBUG)
+    context.file = None
+    context.files = {}
 
 
 def after_all(context):
@@ -22,6 +24,8 @@ def after_feature(context, feature):
 
 def before_scenario(context, scenario):
     benv.before_scenario(context, scenario)
+    context.file = None
+    context.files = {}
 
 
 def after_scenario(context, scenario):
--- features/steps/files.py
+++ features/steps/files.py
@@ -1,6 +1,12 @@
+import os
 from behave import step
+from tempfile import NamedTemporaryFile
 
 
-@step(u'an empty file "foo"')
-def empty_file(context):
-        assert False
+@step(u'an empty file "{name}"')
+def empty_file(context, name):
+    if name not in context.files:
+        context.files[name] = NamedTemporaryFile()
+    context.file = context.files[name]
+    assert os.path.exists(context.file.name), "Path exists"
+    assert os.path.isfile(context.file.name), "Path is file"
