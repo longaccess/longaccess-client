b099d5621e46e6045fa7859cbecace447c3a34af
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2013-09-30 10:09:53 +0300

    Recreate mock S3 buckets when spawning test procs

--- features/steps/__init__.py
+++ features/steps/__init__.py
@@ -4,10 +4,15 @@ from behave_cli.api.steps import *
 from behave_cli.s3mock.steps import *
 from behave_cli.s3mock import setup as s3setup
 from behave_cli.s3mock import teardown as s3teardown
+from behave_cli.s3mock.steps import s3bucket_named
 
 
 def mp_setup(ctx):
+    buckets = ctx.buckets  # save buckets before setup resets them
     s3setup(ctx)
+    if len(buckets):
+        for bucket in buckets:
+            s3bucket_named(ctx, bucket)
 
 
 def mp_teardown(ctx):
