da3b2e1c8c24c34f4b8060d10a2b94262d04015d
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2013-10-01 18:50:58 +0300

    add steps for netrc handling

--- behave_cli/netrc/steps.py
+++ behave_cli/netrc/steps.py
@@ -13,7 +13,14 @@ def the_password(context, password):
     context.password = password
 
 
-@step(u'I store my credentials in "{file}"')
+@step(u'I store my credentials for "{machine}" in "{file}"')
 @format_vars
-def store_in_netrc(context, file):
-    pass
+def store_in_netrc(context, machine, file):
+    netrc = open(file, 'w')
+    assert context.username
+    assert context.password
+    netrc.write("machine {m} login {u} password {p}".format(
+        m=machine,
+        u=context.username,
+        p=context.password))
+    netrc.close()
