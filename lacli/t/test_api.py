ec30f2622026bda347b18188a48592989d31269c
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2013-10-01 15:11:49 +0300

    add unit test for get_capsules in api class

--- lacli/t/test_api.py
+++ lacli/t/test_api.py
@@ -32,3 +32,42 @@ class ApiTest(TestCase):
         api = self._makeit(url="http://baz.com/", session=s)
         self.assertEqual("lala", api.root)
         s.get.assert_called_with("http://baz.com/")
+
+    def test_capsules(self):
+        mattr = {
+            'json.return_value': LA_CAPSULES_RESPONSE,
+            'raise_for_status': None,
+        }
+        mattr = {'get.return_value': Mock(**mattr)}
+        s = Mock(**mattr)
+        api = self._makeit(url="http://baz.com", session=s)
+        capsules = api.get_capsules()
+        self.assertEqual(len(capsules), 2)
+
+
+LA_CAPSULES_RESPONSE = """{
+  "meta": {
+     "limit": 20,
+     "next": null,
+     "offset": 0,
+     "previous": null,
+     "total_count": 2
+  },
+  "objects": [
+     {
+         "created": "2013-06-07T10:45:01",
+         "id": 3,
+         "resource_uri": "/api/v1/capsule/3/",
+         "title": "Photos",
+         "user": "/api/v1/user/3/"
+      },
+      {
+          "created": "2013-06-07T10:44:38",
+          "id": 2,
+          "resource_uri":
+          "/api/v1/capsule/2/",
+          "title": "Stuff",
+          "user": "/api/v1/user/2/"
+       }
+    ]
+}"""
