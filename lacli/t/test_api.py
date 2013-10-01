ccf3454db6f850ca813b5aadf4f4f282614520af
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2013-10-01 15:09:43 +0300

    refactor api unit test to support raise_for_status

--- lacli/t/test_api.py
+++ lacli/t/test_api.py
@@ -11,14 +11,24 @@ class ApiTest(TestCase):
 
     def _makeit(self, *args, **kw):
         from lacli.api import Api
-        return Api(*args,  **kw)
+        return Api(*args, **kw)
+
+    def _makesession(self, rsp, method='get'):
+        mattr = {method+'.return_value': rsp}
+        return Mock(**mattr)
+
+    def _makejson(self, json, **kwargs):
+        mattr = {
+            'json.return_value': json,
+            'raise_for_status': Mock(**kwargs),
+        }
+        return self._makesession(Mock(**mattr))
 
     def test_api(self):
         assert self._makeit()
 
     def test_api_root(self):
-        mattr = {'get.return_value': 'lala'}
-        s = Mock(**mattr)
+        s = self._makejson('lala')
         api = self._makeit(url="http://baz.com/", session=s)
-        self.assertEqual("lala", api.root())
+        self.assertEqual("lala", api.root)
         s.get.assert_called_with("http://baz.com/")
