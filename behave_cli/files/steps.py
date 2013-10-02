d1f783a31b1468bdc6f190129a00adc1b53b94a1
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2013-10-02 14:51:18 +0300

    support for fifo based files

--- behave_cli/files/steps.py
+++ behave_cli/files/steps.py
@@ -1,6 +1,9 @@
 import os
 from behave import step
 from tempfile import NamedTemporaryFile
+from errno import EPIPE
+from multiprocessing import Process
+from tempfile import mkdtemp
 
 
 @step(u'an empty file "{name}"')
@@ -17,3 +20,33 @@ def file_with_content(context, name):
     assert name not in context.files
     empty_file(context, name)
     context.files[name].write(context.text)
+
+
+@step(u'a file "{name}" with {mb} mb of zeroes')
+def file_zeroes(context, name, mb):
+    assert name not in context.dirs
+    assert name not in context.fifo
+    fname = os.path.join(mkdtemp(), 'fifo')
+    os.mkfifo(fname)
+    mb = int(mb)
+
+    def run_fifo():
+        import sys
+        sys.stdout = sys.stderr = open('/tmp/fiflog', 'w')
+        sys.stderr.write("START")
+        fifo = open(fname, 'w')
+        for _ in xrange(mb*1024):
+            try:
+                fifo.write('0' * 1024)
+                print ("wrote 1k\n")
+                sys.stdout.flush()
+                fifo.flush()
+            except IOError as e:
+                if e.errno != EPIPE:
+                    raise
+        fifo.close()
+
+    proc = Process(target=run_fifo)
+    proc.start()
+    context.fifo[fname] = proc
+    context.files[name] = open(fname, 'r')
