323a7324b224420b78a2202dd8a03f81d7227795
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2013-10-03 17:31:17 +0300

    produce a proper netrc file without the port

--- features/steps/basic.py
+++ features/steps/basic.py
@@ -13,4 +13,4 @@ def my_store_in_netrc(context, file):
     p = urlparse(context.mock_api.url())
     context.execute_steps(u"""
         Given I store my credentials for "{}" in "{}"
-        """.format(p.netloc, file))
+        """.format(p.hostname, file))
