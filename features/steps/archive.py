7036f98537786ad036a537cdda0c440b27897e3e
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2013-10-04 18:38:54 +0300

    don't makedirs if dir exists

--- features/steps/archive.py
+++ features/steps/archive.py
@@ -8,6 +8,7 @@ from tempfile import NamedTemporaryFile
 @step(u'I have 1 prepared archive')
 def one_archive(context):
     d = os.path.join(context.environ['HOME'], ".longaccess/archives")
-    os.makedirs(d)
+    if not os.path.isdir(d):
+        os.makedirs(d)
     context.archive = NamedTemporaryFile(dir=d, suffix='.adf')
     context.archive.write(make_adf())
