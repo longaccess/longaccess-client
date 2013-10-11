6d228898d88e03943c2c79d80b65166d490e70b7
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2013-10-11 13:02:43 +0300

    update setup.py with correct packages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from lacli import __version__
 
 setup(version=unicode(__version__),
@@ -6,7 +6,7 @@ setup(version=unicode(__version__),
       author="Konstantinos Koukopoulos",
       description="The Long Access client",
       long_description=open('README').read(),
-      packages=['lacli', 'lacli.t', 'latvm', 'latvm.t'],
+      packages=['lacli', 'lacli.t', 'lacli.cipher'],
       install_requires=['boto', 'python-dateutil', 'filechunkio', 'docopt',
                         'progressbar', 'logutils', 'requests', 'setproctitle',
                         'unidecode', 'pycrypto', 'pyyaml'],
