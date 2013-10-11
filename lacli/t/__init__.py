c8566c9d0706ce6f4fd43a8a4ed46c755e5997bf
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2013-10-11 17:59:28 +0300

    refactor _test_home out

--- lacli/t/__init__.py
+++ lacli/t/__init__.py
@@ -1,6 +1,9 @@
 from boto import set_stream_logger
 from lacli.log import setupLogging
 from binascii import a2b_hex
+from contextlib import contextmanager
+from tempfile import mkdtemp
+from shutil import rmtree
 
 
 def setup():
@@ -30,3 +33,10 @@ dummykey = a2b_hex(
     '824aed71bd74c656ed6bdaa19f2a338faedd824d5fd6e96e85b7fac5c6dabe18')
 
 dummyurl = 'http://download.longaccess.com/x0fs8907494875'
+
+
+@contextmanager
+def _temp_home():
+    d = mkdtemp()
+    yield d
+    rmtree(d)
--- lacli/t/test_cache.py
+++ lacli/t/test_cache.py
@@ -2,10 +2,8 @@
 import os
 
 from testtools import TestCase
-from . import makeprefs, dummykey, dummyurl
-from shutil import rmtree, copy
-from contextlib import contextmanager
-from tempfile import mkdtemp
+from . import makeprefs, dummykey, dummyurl, _temp_home
+from shutil import copy
 from mock import patch, Mock
 
 
@@ -22,12 +20,6 @@ class CacheTest(TestCase):
         from lacli.cache import Cache
         return Cache(*args, **kwargs)
 
-    @contextmanager
-    def _temp_home(self):
-        d = mkdtemp()
-        yield d
-        rmtree(d)
-
     def test_cache(self):
         assert self._makeit("")
 
@@ -40,7 +32,7 @@ class CacheTest(TestCase):
         self.assertEqual(archives[0].title, 'milos 2013')
 
     def test_prepare(self):
-        with self._temp_home() as home:
+        with _temp_home() as home:
             cache = self._makeit(home)
             cache.prepare('foo', os.path.join('t', 'data', 'arc1'))
             archives = cache.archives()
@@ -51,7 +43,7 @@ class CacheTest(TestCase):
         d = 'archives'
         self.assertTrue(
             os.path.isdir(self._makeit(self.home)._cache_dir(d)))
-        with self._temp_home() as home:
+        with _temp_home() as home:
             cache = self._makeit(home)
             self.assertFalse(os.path.exists(cache._cache_dir(d)))
             self.assertTrue(os.path.isdir(cache._cache_dir(d, write=True)))
@@ -60,7 +52,7 @@ class CacheTest(TestCase):
         open_mock = Mock(return_value=None)
         import lacli.cache as cache
         with patch.object(cache, 'open', open_mock, create=True):
-            with self._temp_home() as home:
+            with _temp_home() as home:
                 cache = self._makeit(home)
                 dname = os.path.join(home, 'archives')
                 fname = os.path.join(dname, 'foo')
@@ -81,7 +73,7 @@ class CacheTest(TestCase):
     def test_certs(self):
         cache = self._makeit(self.home)
         self.assertEqual({}, cache.certs())
-        with self._temp_home() as home:
+        with _temp_home() as home:
             cache = self._makeit(home)
             cdir = os.path.join(home, 'certs')
             os.makedirs(cdir)
@@ -104,7 +96,7 @@ class CacheTest(TestCase):
             self.assertEqual('pbkdf2', c.method)
 
     def test_links(self):
-        with self._temp_home() as home:
+        with _temp_home() as home:
             cache = self._makeit(home)
             cdir = os.path.join(home, 'archives')
             os.makedirs(cdir)
