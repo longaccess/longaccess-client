c8ffa62bb6193393bd6a7396ad8c181d0e0b0f7a
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2013-10-22 12:44:54 +0300

    support freeze on windows

--- pyinstall/lacli-script.py
+++ pyinstall/lacli-script.py
@@ -1,6 +1,9 @@
 import lacli.main
 import os
 import sys
+import multiprocessing
+
 os.environ['REQUESTS_CA_BUNDLE'] = os.path.join(
     os.path.dirname(sys.executable), 'cacert.pem')
+multiprocessing.freeze_support()
 lacli.main.main()
