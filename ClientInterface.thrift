fb68715d0a0f3cf715e2457b889a4d6244d460b1
ShadowMech <basilis.lab@gmail.com>
2013-11-30 00:49:25 +0200

    Update ClientInterface.thrift

--- ClientInterface.thrift
+++ ClientInterface.thrift
@@ -1,9 +1,26 @@
-
+//============Errors====================
+enum ErrorType {
+  None = 0,
+  Server = 1,
+  nNetwork = 2,
+  Authentication = 3,
+  Validation = 4,
+  Other = 5
+}
+exception InvalidOperation {
+  1: ErrorType what,
+  2: string why
+}
+struct DateInfo {
+  1: i32 Day,
+  2: i32 Month,
+  3: i32 Year
+}
 //===========Structures=================
-struct TranferStatus {
-  1: SomeException Error,
+struct TransferStatus {
+  1: string StatusDescription, 
   2: string ETA,
-  3: int32 Remaining MB,
+  3: i32 RemainingMB,
   4: double Progress
 }
 struct Capsule {
@@ -12,58 +29,51 @@ struct Capsule {
   3: string Resource_URI,
   4: string Title,
   5: string User,
-  6: DateTime Expires
+  6: DateInfo ExpirationDate
 }
 struct Archive {
   1: string ID,
+}
+struct Certificate {
+  1: string Title,
+  2: string Description,
+  3: string HexDigitsKey
 }
 
 
 //===========Methods===================
 service LongAccessClient {
-  bool LoginUser(string username,string Pass, bool Remember) throws (1:SomeException error),
+  bool LoginUser(1: string username, 2: string Pass,3: bool Remember) throws (1:InvalidOperation error),
   
-  bool Logout() throws (1:SomeException error),
-  
-  
-  list<Capsule> GetCapsules() throws (1:SomeException error)  throws (1:SomeException error),
-  
-  Archive UploadFileGUI(list<string> filePaths,string capsuleID, string title,string description) 
-  throws (1:SomeException error),
-  
-  void GeginUpload(string ArchiveID),
-  
-  TransferStatus QueryArchiveStatus(string ArchiveID),
-  
-  void PauseUpload(string ArchiveID),
-  
-  void ResumeUpload(string ArchiveID),
-  
-  void CancelUpload(string ArchiveID)
+  bool Logout() throws (1:InvalidOperation error),
   
   
-  list<Certificates> getCertificates(),
+  list<Capsule> GetCapsules() throws (1:InvalidOperation error),
+  
+  Archive UploadFileGUI(1: list<string> filePaths,2: string capsuleID, 3: string title, 4: string description) 
+  throws (1: InvalidOperation error),
+  
+  void GeginUpload(1: string ArchiveID),
+  
+  TransferStatus QueryArchiveStatus(1: string ArchiveID),
+  
+  void PauseUpload(1: string ArchiveID),
+  
+  void ResumeUpload(1: string ArchiveID),
+  
+  void CancelUpload(1: string ArchiveID)
+  
+  
+  list<Certificate> getCertificates(),
   
   string GetCertificateFolder(),
   
-  void SetCertificateFolder(string path),
+  void SetCertificateFolder(1: string path),
   
-  binary Export(string certificateID,string format), //format??
+  binary Export(1: string certificateID,2: string format), //format??
   
-  void Decrypt(archivePath, key, destinationPath)
-  
+  void Decrypt(1: string archivePath,2: string key,3: string destinationPath)  
   
 }
 
 
-//============Errors====================
-enum ErrorTypes {
-  ADD = 1,
-  SUBTRACT = 2,
-  MULTIPLY = 3,
-  DIVIDE = 4
-}
-exception SomeException {
-  1: ErrorTypes ErrorType,
-  2: string why
-}
