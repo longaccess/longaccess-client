c611d8010a5f89854d15fb94e860493713cf9635
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2013-12-16 17:45:17 +0200

    log unhandled exceptions

--- lacli/server/error.py
+++ lacli/server/error.py
@@ -1,6 +1,8 @@
 from lacli.exceptions import ApiAuthException
 from lacli.server.interface.ClientInterface.ttypes import InvalidOperation
 from lacli.server.interface.ClientInterface.ttypes import ErrorType
+from lacli.log import getLogger
+from twisted.python import log as twisted_log
 from functools import wraps
 
 
@@ -8,8 +10,9 @@ def tthrow(f):
     """ Decorate a method to raise InvalidOperation instead
         of lacli exceptions
     """
+
     @wraps(f)
-    def wrap(*args, **kwargs):
+    def w(*args, **kwargs):
         try:
             return f(*args, **kwargs)
         except ApiAuthException as e:
@@ -17,6 +20,8 @@ def tthrow(f):
         except NotImplementedError as e:
             raise InvalidOperation(ErrorType.NotImplemented, str(e))
         except Exception as e:
+            getLogger().debug("unhandled exception",
+                              exc_info=True)
+            twisted_log.err(e)
             raise InvalidOperation(ErrorType.Other, "Unknown error")
-
-    return wrap
+    return w
