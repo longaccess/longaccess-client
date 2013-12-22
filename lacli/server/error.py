3a809033788795c124f97bd68e1639e00b557105
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2013-12-22 17:10:28 +0200

    use inlineCallbacks in tthrow

--- lacli/server/error.py
+++ lacli/server/error.py
@@ -4,29 +4,9 @@ from lacli.server.interface.ClientInterface.ttypes import ErrorType
 from lacli.log import getLogger
 from twisted.python import log as twisted_log
 from twisted.python import failure
-from twisted.internet.defer import Deferred
+from twisted.internet import defer
 from functools import wraps
 import errno
-
-
-def makeInvalidOperation(error):
-    try:
-        error.raiseException()
-    except ApiAuthException as e:
-        raise InvalidOperation(ErrorType.Authentication, e.msg)
-    except NotImplementedError as e:
-        raise InvalidOperation(ErrorType.NotImplemented, str(e))
-    except OSError as e:
-        if e.errno == errno.ENOENT:
-            raise InvalidOperation(ErrorType.FileNotFound,
-                                   filename=e.filename)
-    except ValueError as e:
-        raise InvalidOperation(ErrorType.Validation, str(e))
-    except Exception as e:
-        getLogger().debug("unhandled exception",
-                          exc_info=True)
-        twisted_log.err(e)
-        raise InvalidOperation(ErrorType.Other, "Unknown error")
 
 
 def tthrow(f):
@@ -34,16 +14,25 @@ def tthrow(f):
         of lacli exceptions
     """
     @wraps(f)
+    @defer.inlineCallbacks
     def w(*args, **kwargs):
         r = None
         try:
-            r = f(*args, **kwargs)
-            
-            if isinstance(r, Deferred):
-                r.addErrback(makeInvalidOperation)
-            return r
+            r = yield f(*args, **kwargs)
+            defer.returnValue(r)
+        except ApiAuthException as e:
+            twisted_log.err(e)
+            raise InvalidOperation(ErrorType.Authentication, e.msg)
+        except NotImplementedError as e:
+            raise InvalidOperation(ErrorType.NotImplemented, str(e))
+        except OSError as e:
+            if e.errno == errno.ENOENT:
+                raise InvalidOperation(ErrorType.FileNotFound,
+                                       filename=e.filename)
+        except ValueError as e:
+            raise InvalidOperation(ErrorType.Validation, str(e))
         except Exception as e:
-            if isinstance(r, Deferred):
-                r.cancel()
-            return makeInvalidOperation(failure.Failure(e))
+            getLogger().debug("unknown exception", exc_info=True)
+            twisted_log.err(e)
+            raise InvalidOperation(ErrorType.Other, "Unknown error")
     return w
