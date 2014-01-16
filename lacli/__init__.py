e011a13910ba0f92974fb28d9ab37ced70844fd3
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2014-01-16 18:49:24 +0200

    bump version

--- lacli/__init__.py
+++ lacli/__init__.py
@@ -4,7 +4,7 @@ import platform
 try:
     from lacli.version import __version__
 except ImportError:
-    __version__ = "0.2rc2.dev"
+    __version__ = "0.2"
 
 def get_client_info(terse=False):
     el = ["Longaccess client"]
