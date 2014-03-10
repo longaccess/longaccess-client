2eca78018b4451f9165f1399245f2053233e97a9
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2014-03-10 19:18:17 +0200

    add support for reading zip entries from url's. Fixes: #153

--- lacli/t/test_archive.py
+++ lacli/t/test_archive.py
@@ -2,12 +2,15 @@
 import os
 import operator
 
+from StringIO import StringIO
 from testtools import TestCase
 from . import makeprefs
 from shutil import rmtree
 from contextlib import contextmanager
 from tempfile import mkdtemp
-from mock import Mock
+from mock import Mock, patch
+from lacli.adf import Archive, Meta, Certificate
+from zipfile import ZipFile
 
 
 class ArchiveTest(TestCase):
@@ -61,3 +64,36 @@ class ArchiveTest(TestCase):
                         map(operator.itemgetter(1),
                             map(operator.itemgetter(0),
                                 cb.call_args_list)))
+
+    def test_zip_urls(self):
+        murl = Mock(return_value=StringIO("file contents"))
+        with patch('urllib2.urlopen', murl):
+            from lacli.archive.urls import args
+            cb = Mock()
+            a, k = next(args(['http://foobar'], cb))
+            self.assertTrue('arcname' in k)
+            self.assertEqual(str, type(k['arcname']))
+            self.assertEqual('foobar', k['arcname'])
+            self.assertEqual('foobar', cb.call_args[0][1])
+
+    def test_dump_urls(self):
+        fno = {'fileno.return_value': 0,
+               'read.side_effect': ["file contents man", None]}
+        mrsp = Mock(**fno)
+        with patch('urllib2.urlopen', Mock(return_value=mrsp)):
+            with self._temp_home() as tmpdir:
+                from lacli.archive.urls import dump_urls
+                cb = Mock()
+                archive = Archive('foo', Meta(
+                    format='zip', cipher='xor', created='now'))
+                name, path, auth = dump_urls(
+                    archive, ['http://foobar'], Certificate('\0'*8),
+                    cb, tmpdir=tmpdir)
+                self.assertTrue(os.path.exists(path))
+                with ZipFile(path) as zf:
+                    for zi in zf.infolist():
+                        zf.extract(zi, tmpdir)
+                path = os.path.join(tmpdir, "foobar")
+                self.assertTrue(os.path.exists(path))
+                with open(path) as f:
+                    self.assertEqual("file contents man", f.read())
