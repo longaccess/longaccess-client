15a5870755e05f121128a6fba1a0a77bca8bbb28
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2014-03-14 20:00:13 +0200

    in api calls don't send auth unless we have to

--- lacli/api.py
+++ lacli/api.py
@@ -42,7 +42,6 @@ class TwistedRequestsFactory(object):
 
         def _defaults(self, kwds):
             defaults = {
-                'auth': self.session.auth,
                 'persistent': False,
             }
             kwds.setdefault('headers', {}).update({
@@ -52,8 +51,13 @@ class TwistedRequestsFactory(object):
 
         @defer.inlineCallbacks
         def get(self, *args, **kwargs):
-            r = yield treq.get(*args, **self._defaults(kwargs))
-            r = yield self.get_content(r)
+            try:
+                r = yield treq.get(*args, **self._defaults(kwargs))
+                r = yield self.get_content(r)
+            except ApiAuthException:
+                kwargs['auth'] = self.session.auth
+                r = yield treq.get(*args, **self._defaults(kwargs))
+                r = yield self.get_content(r)
             defer.returnValue(r)
 
         @defer.inlineCallbacks
