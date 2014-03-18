41106abbdf7089b4c0e04caadc17e6b4bfe2e51b
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2014-03-18 18:20:59 +0200

    remove unused with_api_response decorator

--- lacli/decorators.py
+++ lacli/decorators.py
@@ -1,8 +1,4 @@
 from functools import update_wrapper, wraps
-from requests.exceptions import ConnectionError, HTTPError
-from lacli.exceptions import (ApiErrorException, ApiAuthException,
-                              ApiUnavailableException,
-                              ApiNoSessionError, BaseAppException)
 
 
 def expand_args(f):
@@ -28,33 +24,6 @@ class cached_property(object):
 
     def __set__(self, obj, value):
         obj.__dict__[self.f.__name__] = value
-
-
-def with_api_response(f):
-    """ Decorate a method to capture API related errors
-    """
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        try:
-            r = f(*args, **kwargs)
-            r.raise_for_status()
-            return r.json()
-        except ConnectionError as e:
-            raise ApiUnavailableException(e)
-        except HTTPError as e:
-            if e.response.status_code == 404:
-                raise ApiUnavailableException(e)
-            elif e.response.status_code == 401:
-                raise ApiAuthException(exc=e)
-            else:
-                raise ApiErrorException(e)
-        except ApiNoSessionError:
-            raise
-        except BaseAppException:
-            raise
-        except Exception as e:
-            raise ApiErrorException(e)
-    return wrap
 
 
 def contains(cls):
