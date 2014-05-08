3582acd74d4d1b8e387a83967e824686c7d869aa
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2014-05-08 15:58:34 +0300

    bump version

--- lacli/__init__.py
+++ lacli/__init__.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 try:
     from lacli.version import __version__
 except ImportError:
-    __version__ = "0.2.6"
+    __version__ = "0.2.6.1"
 
 
 def get_client_info(terse=False):
